db8d4c62f61c53a4384bf513a0291e248336b2c7
Peter Aspiazu <paspiazusabando@gmail.com>
2025-09-07 15:28:54 -0500

    eliminando todos los estilos de globals.css para evitar el bloqueo de renderizacion y refactorizando a tailwind

--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -2,7 +2,7 @@
 export const Footer = () => {
   return (
     // FOOTER
-    <footer>
+    <footer className="bg-[#212529] text-white py-8 px-4 text-center">
         <p>© 2025 Puertas Automatizadas Todos los derechos reservados.</p>
     </footer>
   )
